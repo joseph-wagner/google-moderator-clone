e45e02a2c75d22b71cb9878776bc42e3a6fbd8f4
Robert Helmick <robert.helmick.08@gmail.com>
2017-08-29 13:48:19 -0400

    series object change

---
+++ js/series.js
@@ -0,0 +1,141 @@
+var seriesList = new Vue({
+  el: '.series-list',
+  data: {
+    series: [{
+        title: 'Topic 1',
+        questions: [{
+          title: "T1 Q1"
+        },
+        {
+          title: "T1 Q2"
+        },
+        {
+          title: "T1 Q3"
+        },
+        {
+          title: "T1 Q4"
+        }]
+      },
+      {
+        title: 'Topic 2',
+        questions: [{
+          title: "T2 Q1"
+        },
+        {
+          title: "T2 Q2"
+        },
+        {
+          title: "T2 Q3"
+        },
+        {
+          title: "T2 Q4"
+        }]
+      },
+      {
+        title: 'Topic 3',
+        questions: [{
+          title: "T3 Q1"
+        },
+        {
+          title: "T3 Q2"
+        },
+        {
+          title: "T3 Q3"
+        },
+        {
+          title: "T3 Q4"
+        }]
+      },
+      {
+        title: 'Topic 4',
+        questions: [{
+          title: "T4 Q1"
+        },
+        {
+          title: "T4 Q2"
+        },
+        {
+          title: "T4 Q3"
+        },
+        {
+          title: "T4 Q4"
+        }]
+      },
+      {
+        title: 'Topic 5',
+        questions: [{
+          title: "T5 Q1"
+        },
+        {
+          title: "T5 Q2"
+        },
+        {
+          title: "T5 Q3"
+        },
+        {
+          title: "T5 Q4"
+        }]
+      },
+      {
+        title: 'Topic 6',
+        questions: [{
+          title: "T6 Q1"
+        },
+        {
+          title: "T6 Q2"
+        },
+        {
+          title: "T6 Q3"
+        },
+        {
+          title: "T6 Q4"
+        }]
+      },
+      {
+        title: 'Topic 7',
+        questions: [{
+          title: "T7 Q1"
+        },
+        {
+          title: "T7 Q2"
+        },
+        {
+          title: "T7 Q3"
+        },
+        {
+          title: "T7 Q4"
+        }]
+      },
+      {
+        title: 'Topic 8',
+        questions: [{
+          title: "T8 Q1"
+        },
+        {
+          title: "T8 Q2"
+        },
+        {
+          title: "T8 Q3"
+        },
+        {
+          title: "T8 Q4"
+        }]
+      },
+      {
+        title: 'Topic 9',
+        questions: [{
+          title: "T9 Q1"
+        },
+        {
+          title: "T9 Q2"
+        },
+        {
+          title: "T9 Q3"
+        },
+        {
+          title: "T9 Q4"
+        }]
+      }
+    ]
+  }
+});
